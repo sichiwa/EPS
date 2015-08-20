eab75acc247f4fe07cd517ea45835039aef75559
fuyen <fuyen721212@gmail.com>
2015-08-20 14:38:58 +0800

    2015/08/20 初始化EPS專案

---
+++ eps/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EPS.Startup))]
+namespace EPS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
